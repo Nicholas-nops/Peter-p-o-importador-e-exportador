0d8d520b1444ac1f4e9ac2dff4aa79c33211bca6
Your Name <you@example.com>
2019-09-25 13:51:52 -0300

    create mode 100644 leiame.txt Programa inicia do projeto

---
+++ Situacao02/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Situacao02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTimport = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.LBlista = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.BTimport, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LBlista, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(520, 384);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Importar Dados";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BTimport
+            // 
+            this.BTimport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BTimport.Location = new System.Drawing.Point(3, 53);
+            this.BTimport.Name = "BTimport";
+            this.BTimport.Size = new System.Drawing.Size(167, 44);
+            this.BTimport.TabIndex = 1;
+            this.BTimport.Text = "Importar";
+            this.BTimport.UseVisualStyleBackColor = true;
+            this.BTimport.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(349, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 50);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Exportar Dados";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(349, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exportar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LBlista
+            // 
+            this.LBlista.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LBlista.FormattingEnabled = true;
+            this.LBlista.Location = new System.Drawing.Point(3, 103);
+            this.LBlista.Name = "LBlista";
+            this.LBlista.Size = new System.Drawing.Size(167, 278);
+            this.LBlista.TabIndex = 4;
+            this.LBlista.SelectedIndexChanged += new System.EventHandler(this.LBlista_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(176, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 50);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Pete\'s Import And Exporter";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFile_FileOk);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(176, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 50);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Quais Items você deseja importar?";
+            // 
+            // saveFile
+            // 
+            this.saveFile.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.checkBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox4, 0, 3);
+            this.tableLayoutPanel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(176, 103);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(167, 278);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "CPF";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(3, 53);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(58, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "EMAIL";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 103);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(132, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "DATA DE REGISTRO";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(3, 153);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(37, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "ID";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 384);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTimport;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox LBlista;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ Situacao02/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Situacao02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void AtualizaListbox() {
+
+            LBlista.Items.Clear();
+
+            foreach (var item in importador)
+            {
+               LBlista.Items.Add(item.Cliente);
+            }
+        
+        }
+        class Importer
+        {
+
+            public string Cliente;
+            public string Data;
+            public string CPF;
+            public string email;
+            public string ID;
+        }
+        List<Importer> importador = new List<Importer>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+
+            
+        }
+
+        private void openFile_FileOk(object sender, CancelEventArgs e)
+        {
+            using (StreamReader leitor = new StreamReader(openFile.FileName))
+            {
+               string line;
+               leitor.ReadLine();
+                while ((line = leitor.ReadLine()) != null)
+                {               
+                    string[] dados = line.Split(';');
+                    Importer tipos = new Importer();
+                    tipos.Cliente = dados[0];
+                    tipos.CPF = dados[1];
+                    tipos.Data = dados[2];
+                    tipos.email = dados[3];
+                    tipos.ID = dados[4];
+                    importador.Add(tipos);
+
+                }
+                AtualizaListbox();
+
+            }
+        }
+
+        private void LBlista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           foreach (var item in importador)
+	        {
+                if (item.Cliente == LBlista.SelectedItem)
+                {
+                    MessageBox.Show("CPF: " + item.CPF + "|" + "Data De Registro: " + item.Data + "|" + "Email: " + item.email + "|" + "ID: " + item.ID + "|");
+                }
+           }          
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+            using (StreamWriter writer = new StreamWriter(saveFile.FileName, false))
+            {
+                foreach (Importer item in importador)
+                {
+                    string aux = item.Cliente;
+                    if (checkBox1.Checked)
+                    {
+                        aux += ";" + item.CPF;
+                    }
+                    if (checkBox2.Checked)
+                    {
+                        aux += ";" + item.email;
+                    }
+                    if (checkBox3.Checked)
+                    {
+                        aux += ";" + item.Data;
+                    }
+                    if (checkBox4.Checked)
+                    {
+                        aux += ";" + item.ID;
+                    }
+                    writer.WriteLine(aux);
+
+                }
+            }
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFile.ShowDialog();
+        }
+
+        private void CheckedBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+                
+
+            }
+            
+        
+    } 
+}
